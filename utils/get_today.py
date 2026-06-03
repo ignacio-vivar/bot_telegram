5906cdfd3303ccb269d070047ec48d6185c4b5f8
ignacio <jjivc5@gmail.com>
2026-06-02 22:14:17 -0300

    time

--- utils/get_today.py
+++ utils/get_today.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
+ARG_TZ = ZoneInfo("America/Argentina/Buenos_Aires")
 
 def get_today():
-    fecha_actual = datetime.now().date()
+    fecha_actual = datetime.now(ARG_TZ).date()
     fecha_epoca = datetime(1970, 1, 1).date()
-    delta_days = (fecha_actual - fecha_epoca).days
 
-    return delta_days
+    return (fecha_actual - fecha_epoca).days
